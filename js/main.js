b5677683d73b809b214abbd7294156174009c20f
Jhenry Sierra <jhenrims@gmail.com>
2023-03-19 13:22:27 -0400

    Arreglos de CSS, implementacion del localStorage, seccion de calculos guardados git status

--- js/main.js
+++ js/main.js
@@ -1,106 +1,22 @@
 // Calculadora de Cuotas 
 
 /*
-1)_Preguntar por prompt el nombre del usuario
-2)_Preguntar por prompt el precio del producto (asegurarse de convertir a numero con "Number")
-3)_Preguntar por descuento
-4)_Preguntar por prompt la tasa de interes anual.
-5)_Preguntar por prompt la cantidad de cuotas
-6)_Realizar calculos internamentes
-7)_Almacenar datos como Objetos dentro de un Array
-8)_Boton de filtrar por cuotas
+1)_Formulario que pide datos al usuario para calcular cuotas
+2)_Pide Nombre, Precio del producto, descuento, tasa y cuotas deseadas.
+3)_Hace los calculos internos y muestra los resultados por HTML
+4)_Posee botones con distintas funciones como Calcular, Limpiar Formulario y Guardar en localStorage
+5)_Posee una seccion de Calculos Guardados que muestra los calculos guardados en localStorage
 */
 
-//Un array vacio, el cual almacenara los datos de calculos hechos
-let calculos = [];
-/*
-//Funcion que pedira los datos del usuario y del calculo deseado
-function calcularCuotas() {
-    let datos = {};
-    datos.nombre = prompt(`Bienvenido a Calculadora de Cuotas \n\nEsta herramienta te ayudará a calcular cuotas de cualquier producto en base a su Precio Inicial, Descuento (si aplica), Tasa de Interes y Cantidad de cuotas. \n\nPara iniciar tu calculo, ingresa tu nombre:`);
-    while (!datos.nombre) {
-        datos.nombre = prompt(`Bienvenido a Calculadora de Cuotas! \nEsta herramienta te ayudará a calcular cuotas de cualquier producto en base a su Precio Inicial, Descuento (si aplica), Tasa de Interes y Cantidad de cuotas. \n\nPara iniciar tu calculo, favor de ingresar tu nombre nuevamente:`);
-    }
-
-    datos.precio = Number(prompt(`Gracias ${datos.nombre}, \n \nIngrese el precio del producto deseado: `));
-    while (!datos.precio) {
-        datos.precio = Number(prompt(`Entrada Invalida, Ingrese el precio del producto deseado: `));
-    }
-
-    datos.descuento = Number(prompt(`Introduzca descuento ofrecido (si no tiene descuento salte al siguiente paso)`));
-    while (isNaN(datos.descuento)) {
-        datos.descuento = Number(prompt(`Introduzca descuento ofrecido (si no tiene descuento salte al siguiente paso)`));
-    }
-
-    datos.precioFinal = datos.precio - datos.descuento;
-
-    datos.tasa = Number(prompt(`Precio Final: $${datos.precioFinal}\n\n\nIngrese la tasa de interes anual`));
-    while (!datos.tasa) {
-        datos.tasa = Number(prompt(`Precio Final: $${datos.precioFinal}\n\n\nEntrada Invalida. Ingrese la tasa de interes anual `));
-    }
-
-    datos.interes = datos.precioFinal * datos.tasa / 100;
-
-    datos.cuotas = Number(prompt(`Precio Final: $${datos.precioFinal}\nTasa: ${datos.tasa}%\nTotal: $${datos.precioFinal + datos.interes}\n\nIngrese la cantidad de cuotas`));
-
-    while (!datos.cuotas) {
-        datos.cuotas = Number(prompt(`Precio Final: $${datos.precioFinal}\nTasa: ${datos.tasa}% \n \nEntrada Invalida. Ingrese la cantidad de cuotas`));
-    }
-
-    datos.montoCuota = ((datos.precioFinal + datos.interes) / datos.cuotas).toFixed(2);
-
-    let resultado = (datos.precioFinal + datos.interes) / datos.cuotas;
-    resultado = resultado.toFixed(2);
-
-    let mensaje = `Gracias ${datos.nombre}, \n\nSu producto de $${datos.precio}, con un descuento de $${datos.descuento}, queda en un precio final de $ ${datos.precioFinal}, con una tasa de interes de ${datos.tasa}%, equivale a ${datos.cuotas} cuotas de $${resultado} mensual\n\n`;
-
-    let desglose = `Precio: $${datos.precio}\nDescuento: $${datos.descuento}\nPrecio Final: $${datos.precioFinal}\nTasa: ${datos.tasa}%\nCantidad de cuotas: ${datos.cuotas}\nMonto por cuota: $${resultado}\n\nHaz click Aqui para solicitar prestamo`;
-
-    //Mensaje alert que mostrara el mensaje + desglose
-    alert(mensaje + desglose);
-
-    //Mostrar mensaje dentro del HTML
-    document.getElementById('title').innerHTML = `Bienvenido ${datos.nombre}!`;
-    document.getElementById('desglose').innerHTML = `
-        Vea debajo el desglose de su calculo de sus cuotas:
-        <br><br>- Precio: $${datos.precio}
-        <br>- Descuento: $${datos.descuento}
-        <br>- Precio Final: $${datos.precioFinal}
-        <br>- Tasa: ${datos.tasa}% 
-        <br>- Cantidad de cuotas: ${datos.cuotas}
-        <br> - Monto por cuota: $${resultado}
-        <br><br>`;
-
-    //Objeto'datos' se agregua dentro del Array'calculos' y console log de Array'calculos'
-    calculos.push(datos);
-    console.log(calculos);
-}
-
-//Funcion que crea el filtro por cuotas
-function filtrarCuotas(data) {
-    let cuotaFiltrada = Number(prompt(`Ingrese la cantidad de cuotas a filtrar: `));
-    let dataFiltrada = data.filter(item => item.cuotas === cuotaFiltrada);
-    console.log(`Calculos filtrados por cantidad de cuotas: ${cuotaFiltrada}`, dataFiltrada);
-}
-
-//Hacer que el boton 'filtrarCuotas' ejecute la funcion filtrarCuotas (Esto lo hice dentro de una funcion anonima porque la funcion 'filtrarCuotas' se ejecutaba desde que se ejecutaba el programa y no esparaba que de click al boton)
-document.getElementById('filtrarCuotas').addEventListener('click', function () {
-    filtrarCuotas(calculos);
-});
-
-//Hacer que boton 'calcularBtn' ejecute la funcion 'calcularCuotas'
-document.getElementById('calcularBtn').addEventListener('click', calcularCuotas)
-*/
-
-
-//Funcion que me captura la informacion de los inputs
-
+//Selecciona elementos HTML
 let form = document.getElementById('form');
 let calcularBtn = document.getElementById('calcularBtn');
 let limpiar = document.getElementById('limpiar')
 let guardar = document.getElementById('guardar');
 let resultados = document.getElementById('resultados')
+let contenedorCalculos = document.getElementById('contenedorCalculos')
 
+//Funcion que me captura la informacion de los inputs
 calcularBtn.addEventListener('click', () => {
     //Capturar valores del formulario
     let nombre = document.getElementById('nombre').value;
@@ -116,10 +32,8 @@ calcularBtn.addEventListener('click', () => {
     let montoCuota = ((precioFinal + interes) / cuotas).toFixed(2);
 
     // Verificar si los campos tienen datos validos
-    if (isNaN(parseFloat(precio)) || isNaN(parseFloat(descuento)) || isNaN(parseFloat(tasa)) || isNaN(parseInt(cuotas))) {
-        alert('Por favor, ingrese datos válidos en los campos correspondientes.');
-        return;
-    }
+    typeof nombre !== 'string' || isNaN(parseFloat(precio)) || isNaN(parseFloat(descuento)) || isNaN(parseFloat(tasa)) || isNaN(parseInt(cuotas)) ? alert('Por favor, ingrese datos válidos en los campos correspondientes.') : null;
+
 
     //Guardar los datos dentro de un objeto
     let datos = {
@@ -142,31 +56,80 @@ calcularBtn.addEventListener('click', () => {
 
     //Mostrar resultados en HTML
     resultados.innerHTML = `
-    <strong>Nombre:</strong>              ${nombreHTML.replace(/"/g, '')} <br>
-    <strong>Precio:</strong>              $${precioHTML.replace(/"/g, '')} <br>
-    <strong>Descuento:</strong>           $${descuentoHTML.replace(/"/g, '')} <br>
-    <strong>Precio Final:</strong>        $${precioFinal} <br>
-    <strong>Tasa de Interes:</strong>     ${tasaHTML.replace(/"/g, '')}% <br>
-    <strong>Interes Total:</strong>       $${interes} <br>
-    <strong>Cantidad de Cuotas:</strong>  ${cuotasHTML.replace(/"/g, '')} <br>
-    <strong>Monto por Cuota:</strong>     $${montoCuota}
+    <strong>Nombre:</strong> ${nombreHTML.replace(/"/g, '')} <br>
+    <strong>Precio:</strong> $${precioHTML.replace(/"/g, '')} <br>
+    <strong>Descuento:</strong> $${descuentoHTML.replace(/"/g, '')} <br>
+    <strong>Precio Final:</strong> $${precioFinal} <br>
+    <strong>Tasa de Interes:</strong> ${tasaHTML.replace(/"/g, '')}% <br>
+    <strong>Interes Total:</strong> $${interes} <br>
+    <strong>Cantidad de Cuotas:</strong> ${cuotasHTML.replace(/"/g, '')} <br>
+    <strong>Monto por Cuota:</strong> $${montoCuota}
     `
 
+    //Guardar en localStorage, clave Nombre y valor obj Datos pasados a json
     guardar.addEventListener('click', () => {
         localStorage.setItem(nombre, JSON.stringify(datos));
+        actualizarContenedorCalculos();
 
     })
 }
 );
 
+//Event listener para boton de Limpiar, que limpia el formulario
 limpiar.addEventListener('click', () => {
     form.reset();
 })
 
+// Recuperar Datos del localStorage
+for (let i = 0; i < localStorage.length; i++) {
+    let key = localStorage.key(i);
+    let data = JSON.parse(localStorage.getItem(key));
 
+    // Generar HTML para cada obj en localStorage
+    let itemHTML = `
+        <div class="card">
+            <div class="card-body calculoGuardado">
+                <h5 class="card-title"><strong>Nombre: ${data.nombre}</strong></h5>
+                <p class="card-text"><strong>Precio:</strong> $${data.precio}</p>
+                <p class="card-text"><strong>Descuento:</strong> $${data.descuento}</p>
+                <p class="card-text"><strong>Precio Final:</strong> $${data.precioFinal}</p>
+                <p class="card-text"><strong>Tasa de Interes:</strong> ${data.tasa}%</p>
+                <p class="card-text"><strong>Interes Total:</strong> $${data.interes}</p>
+                <p class="card-text"><strong>Cantidad de Cuotas:</strong> ${data.cuotas}</p>
+                <p class="card-text"><strong>Monto por Cuota:</strong> $${data.montoCuota}</p>
+            </div>
+        </div>
+    `;
 
+    // Incluir el HTML generado dentro del contenedor HTML
+    contenedorCalculos.innerHTML += itemHTML;
+}
 
+//Generar card para los Obj en localStorage para cuando se guarde calculo nuevo
+function generarCardCalculo(key, data) {
+    return `
+    <div class="card calculoGuardado">
+        <div class="card-body ">
+            <h5 class="card-title"><strong>Nombre: ${data.nombre}</strong></h5>
+            <p class="card-text"><strong>Precio:</strong> $${data.precio}</p>
+            <p class="card-text"><strong>Descuento:</strong> $${data.descuento}</p>
+            <p class="card-text"><strong>Precio Final:</strong> $${data.precioFinal}</p>
+            <p class="card-text"><strong>Tasa de Interes:</strong> ${data.tasa}%</p>
+            <p class="card-text"><strong>Interes Total:</strong> $${data.interes}</p>
+            <p class="card-text"><strong>Cantidad de Cuotas:</strong> ${data.cuotas}</p>
+            <p class="card-text"><strong>Monto por Cuota:</strong> $${data.montoCuota}</p>
+        </div>
+    </div>
+    `;
+}
 
-
-
-
+// Actualizar contenedor HTML "contenedorCalculos"  con los datos del local Storage
+function actualizarContenedorCalculos() {
+    contenedorCalculos.innerHTML = '';
+    for (let i = 0; i < localStorage.length; i++) {
+        let key = localStorage.key(i);
+        let data = JSON.parse(localStorage.getItem(key));
+        let itemHTML = generarCardCalculo(key, data);
+        contenedorCalculos.innerHTML += itemHTML;
+    }
+}
